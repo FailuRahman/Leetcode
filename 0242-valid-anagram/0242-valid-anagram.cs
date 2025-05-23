ca333bb2c2625d0780954222a688bc2743eaca8a
FailuRahman <failuzz00@gmail.com>
2025-05-23 10:00:24 +0530

    Time: 1 ms (85.81%), Space: 43.6 MB (66.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+            return false;
+
+        var count = new int[26]; 
+
+        foreach (char c in s)
+            count[c - 'a']++;
+
+        foreach (char c in t)
+            count[c - 'a']--;
+
+        foreach (int val in count)
+            if (val != 0)
+                return false;
+
+        return true;
+    }
+}
