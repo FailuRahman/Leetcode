d16978a35522ca1e9e8ca0463f939e0ca86c870b
Muhammed Failu Raheman <failuzz00@gmail.com>
2025-05-28 18:57:37 +0530

    Time: 10 ms (10.08%), Space: 41.1 MB (6.48%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (string.IsNullOrEmpty(needle)) {
+            return 0;
+        }
+        return haystack.IndexOf(needle);
+    }
+}
