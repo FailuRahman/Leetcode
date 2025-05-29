81b554ef6aefc905f59ddef58e51d41412bc486e
Muhammed Failu Raheman <failuzz00@gmail.com>
2025-05-29 10:57:25 +0530

    Time: 0 ms (100%), Space: 42.7 MB (39.14%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        
+      int result = 0;
+      foreach(char c in s){
+        result ^= c;
+      }
+
+      foreach(char c in t){
+        result ^= c;
+      }
+      return (char)result;
+    }
+}
