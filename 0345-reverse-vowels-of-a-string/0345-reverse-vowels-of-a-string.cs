f9214f502aacf3ff7b8dae2ba4c7ba979c4aa259
Muhammed Failu Raheman <failuzz00@gmail.com>
2025-05-30 09:50:50 +0530

    Time: 5 ms (55.71%), Space: 45.3 MB (65.04%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string ReverseVowels(string s) {
+        List<char> vowels = new List<char>();
+        
+   
+        foreach (char c in s) {
+            if ("aeiouAEIOU".Contains(c)) {
+                vowels.Add(c);
+            }
+        }
+
+      
+        int vowelIndex = vowels.Count - 1;
+        char[] result = s.ToCharArray();
+
+        for (int i = 0; i < result.Length; i++) {
+            if ("aeiouAEIOU".Contains(result[i])) {
+                result[i] = vowels[vowelIndex--];
+            }
+        }
+
+        return new string(result);
+    }
+}
