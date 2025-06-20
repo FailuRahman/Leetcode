46e8026b83aea0234f2814bd0964a56df24fe0e8
Muhammed Failu Raheman <failuzz00@gmail.com>
2025-06-20 08:53:57 +0530

    Time: 1 ms (46.76%), Space: 52.3 MB (5.8%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] SortArrayByParity(int[] nums) {
+        List<int> even = new List<int>();
+        List<int> odd = new List<int>();
+
+        foreach (int num in nums)
+        {
+
+            if (num % 2 == 0)
+                even.Add(num);
+            else
+                odd.Add(num);
+        }
+
+          even.AddRange(odd); 
+
+        return even.ToArray();
+    }
+}
